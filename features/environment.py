a876b63593fac0bfe1bdbfb9361abc548feaf0e2
Reza Rastgoo <modeeran@gmail.com>
2024-05-06 23:20:18 +0200

    Update environment.py adding os capability

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+import os
 
 def before_scenario(context,driver):
     tag_list = context.tags
     if 'login' in tag_list or 'status' in tag_list:
-        context.driver = webdriver.Chrome()
+
+        executor = os.getenv("SELENIUM_EXECUTOR", "")
+
+        if executor == "container":
+            options = webdriver.ChromeOptions()
+            context.driver = webdriver.Remote(command_executor="http://localhost:4444", options=options)
+
+        elif executor == "" or executor == "local":
+            context.driver = webdriver.Chrome()
+
         context.driver.maximize_window()
         context.driver.get("https://the-internet.herokuapp.com/")
 
